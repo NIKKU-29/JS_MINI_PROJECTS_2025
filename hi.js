822d7429be24cc3aaf2b1ff40d6bd369d0bb2222
Nemish <nemishsbce29@gmail.com>
2025-02-04 17:45:26 +0530

    done this one

--- hi.js
+++ hi.js
@@ -1,1 +1,9 @@
-console.log(`nikku`);
+
+const name=`Nemish`;
+
+
+const bankbalance = new Number(200000);
+console.log(name);
+console.log(typeof(name));
+console.log(bankbalance.toString());
+console.log(typeof(bankbalance));
